bc841dbb08007fc3ef2b631def225cc2efe559a4
Sushant Ranjan <sushantranjan.124139@marwadiuniversity.ac.ingit config --listcleargit config user.namecancelgit config --list git config --global user.email sushantranjan.124139@marwadiuniversity.ac.inclscrbrew --versionecho Hello>
2026-04-13 00:08:25 +0530

    fix api url

--- Backend/server.js
+++ Backend/server.js
@@ -8,8 +8,11 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true,
+  origin: [
+    "http://localhost:5173",
+    "https://mrsushantranjan.github.io"
+  ],
+  credentials: true
 }));
 app.use(express.json());
 
