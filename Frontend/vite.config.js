cbf55438c9934e54f1521c28c58b720c72b292cf
Sushant Ranjan <sushantranjan.124139@marwadiuniversity.ac.ingit config --listcleargit config user.namecancelgit config --list git config --global user.email sushantranjan.124139@marwadiuniversity.ac.inclscrbrew --versionecho Hello>
2026-04-15 16:05:28 +0530

    fix: correct base path for vercel

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/LMS-USING-REACT/',   // 🔥 MUST MATCH REPO NAME
+  base: "/",   // ✅ MUST BE THIS
 })
